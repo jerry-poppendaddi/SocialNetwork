f50d428d65f6dd8ad52338c4cd9067a856bd3513
Ive Alex <ive.alex.89@gmail.com>
2024-06-05 20:58:00 -0400

    First Commit

--- SocialNetwork/Program.cs
+++ SocialNetwork/Program.cs
@@ -1,11 +1,57 @@
-﻿using System;
+﻿using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using System;
 
 namespace SocialNetwork
 {
     class Program
     {
-        public static void Main(string[] args)
+        public static UserService userService = new UserService();
+        static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the Social Network!");
+
+            while (true)
+            {
+                Console.Write("To Register, please enter your First Name: ");
+
+                string firstName = Console.ReadLine();
+
+                Console.Write("Please enter your Last Name: ");
+                string lastName = Console.ReadLine();
+
+                Console.Write("Please create a password: ");
+                string password = Console.ReadLine();
+
+                Console.Write("Please enter your email address: ");
+                string email = Console.ReadLine();
+
+                UserRegistrationData userRegistrationData = new UserRegistrationData()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Password = password,
+                    Email = email
+                };
+
+                try
+                {
+                    userService.Register(userRegistrationData);
+                    Console.WriteLine("Registration successful!");
+                }
+
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine("Please input correct values.");
+                }
+
+                catch (Exception)
+                {
+                    Console.WriteLine("Registration error has occured.");
+                }
+
+                Console.ReadLine();
+            }
 
         }
     }
